034a2a81d0b63d68615555829f6bd25fe954b373
Thomas Riccardi <riccardi.thomas@gmail.com>
2021-10-19 01:06:40 +0200

    Fix linking of libalgobsec.a
    Undefined symbols:
    pow
    fmaxf
    fminf
    fabsf
    floor
    ceil
    fabs
    
    micropython provides its own math libs in lib/libm/ (not sure why)
    but:
    - some are from C99: fminf, fmaxf, fabsf; not provided by micropython
    - some are double precision, not provided by micropython as it's a
      single precision build, for esp8266.
    
    Current solution: link with toolchain libm.a; it only takes the
    missing functions, all the rest is still from micropython lib/libm/

--- modules/bsec/micropython.mk
+++ modules/bsec/micropython.mk
@@ -10,8 +10,10 @@ CFLAGS_USERMOD += -I$(BSEC_MOD_DIR)
 CFLAGS_USERMOD += -I$(BSEC_MOD_DIR)/API
 CFLAGS_USERMOD += -I$(BSEC_MOD_DIR)/algo/bin
 
-# replace that by a copy, adapted to write to micropython I2C
-#SRC_USERMOD += API/example/bsec_integration.c
-
-CFLAGS_USERMOD += -L$(BSEC_MOD_DIR)/algo/bin -lalgobsec
-#CFLAGS_USERMOD += -lm -lrt
+# Link with libalgobsec.a from Bosch
+LDFLAGS_USERMOD += -L$(BSEC_MOD_DIR)/algo/bin -lalgobsec
+# libalgobsec.a requires both C99 math functions such as fminf, not present in micropython/lib/libm/, as well as double precision math functions such as pow
+# => link with compiler libm.a to solve these few missing symbols, the rest of math.h still comes from micropython
+# next line doesn't work, probably because this file is loaded too early; so hardcode the path instead, assuming build from docker with esp-sdk wrapper; adapt according to your local toolchain setup
+#LDFLAGS_USERMOD += -L$(ESP_SDK)/../lib -lm
+LDFLAGS_USERMOD += -L/tools/xtensa-lx106-elf/xtensa-lx106-elf/sysroot/lib -lm
